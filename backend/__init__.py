ddad09353765a385792e8a9cb250ad872803858b
AustinWRye <ryeaustinw@gmail.com>
2019-07-04 21:23:29 +0000

    Add a config file

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,17 @@
+import os
 from flask import Flask
+
+from backend.config import (
+    DevelopmentConfig,
+    ProductionConfig,
+    TestingConfig
+)
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig if os.environ.get(
+        'FLASK_DEBUG') else ProductionConfig)
 
     from backend.main.routes import main
     app.register_blueprint(main)
