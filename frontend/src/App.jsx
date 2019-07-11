c472d08135e31158e56a22e90045cf7b33d53fc7
AustinWRye <ryeaustinw@gmail.com>
2019-07-11 03:03:37 +0000

    Add media queries for resizability support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import Intro from "./containers/intro/Intro";
-import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 import About from "./containers/about/About";
-import Router from "./components/Router";
-import Footer from "./components/Footer";
+// import Router from "./components/Router";
+// import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
     return (
         <BrowserRouter>
             <div id="content">
-                <Intro />
-                <Navbar />
+                <div className="main-bg">
+                    <Intro />
+                    <Header />
+                </div>
                 <About />
                 {/* <Router /> */}
             </div>
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,86 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Row, Col } from "react-bootstrap";
+
+import RyeLogo from "../../icons/rye-logo.png";
+import "./Header.css";
+
+class Header extends Component {
+    state = {
+        sticky: null
+    };
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+        window.addEventListener("resize", this.handleResize);
+        this.setState({ sticky: this.refs.header.offsetTop });
+    }
+
+    handleResize = () => {
+        if (this.refs.header.classList.contains("sticky")) {
+            this.setState({ sticky: this.refs.headerFill.offsetTop });
+        } else {
+            this.setState({ sticky: this.refs.header.offsetTop });
+        }
+    };
+
+    handleScroll = () => {
+        if (window.pageYOffset >= this.state.sticky) {
+            this.refs.header.classList.add("sticky");
+            this.refs.headerFill.classList.add("fill");
+        } else {
+            this.refs.header.classList.remove("sticky");
+            this.refs.headerFill.classList.remove("fill");
+        }
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <div id="header" ref="header">
+                    <Navbar expand="sm">
+                        <Container>
+                            <Row>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Col>
+                                        <Link to="#about" className="link">
+                                            ABOUT
+                                            <div className="link-select" />
+                                        </Link>
+                                    </Col>
+                                    <Col>
+                                        <Link to="#projects" className="link">
+                                            PROJECTS
+                                            <div className="link-select" />
+                                        </Link>
+                                    </Col>
+                                    <Col className="logo">
+                                        <Link to="#intro" className="link">
+                                            <img src={RyeLogo} alt="" />
+                                        </Link>
+                                    </Col>
+                                    <Col>
+                                        <Link to="#skills" className="link">
+                                            SKILLS
+                                            <div className="link-select" />
+                                        </Link>
+                                    </Col>
+                                    <Col>
+                                        <Link to="#work" className="link">
+                                            WORK
+                                            <div className="link-select" />
+                                        </Link>
+                                    </Col>
+                                </Navbar.Collapse>
+                            </Row>
+                        </Container>
+                    </Navbar>
+                </div>
+                <div className="header-fill" ref="headerFill" />
+            </Fragment>
+        );
+    }
+}
+
+export default Header;
